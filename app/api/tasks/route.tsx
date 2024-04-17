eec32d0e1877512401921324f6089794b8c2b9a2
Hanny <cheershanny@gmail.com>
2024-04-17 19:46:02 +0100

    add get and post

--- app/api/tasks/route.tsx
+++ app/api/tasks/route.tsx
@@ -1,5 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json("say hi")
+export async function GET(request: NextRequest) {
+  const tasks = await prisma.task.findMany();
+  return NextResponse.json(tasks);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const newTask = await prisma.task.create({
+    data: { name: body.task },
+  });
+  return NextResponse.json(newTask, { status: 201 });
 }
