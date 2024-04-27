c89db9d4155963bc373b870a7c110120d01b9a58
Hanny Tran <cheershanny@gmail.com>
2024-04-27 16:03:44 +0100

    add validation

--- app/api/tasks/route.tsx
+++ app/api/tasks/route.tsx
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import { taskSchema } from "@/app/types/Task";
+
 
 export async function GET(request: NextRequest) {
   const tasks = await prisma.task.findMany();
@@ -8,8 +10,11 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  const validation = taskSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   const newTask = await prisma.task.create({
-    data: { name: body.task },
+    data: { name: body.name },
   });
   return NextResponse.json(newTask, { status: 201 });
 }
